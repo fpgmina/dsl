204e85282a2fd316ee3535f9c6927d3573ddcca0
fpgmina <francesco.mina1986@gmail.com>
2025-06-05 10:21:05 -0400

    up getters

--- data/getters.py
+++ data/getters.py
@@ -1,11 +1,17 @@
 import pandas as pd
+import pathlib
+
+
+ROOT_PATH = pathlib.Path("/Volumes/Samsung SSD 990 PRO 1TB/data/dsl")
+
+
 
 
 def get_train_data() -> pd.DataFrame:
-    df = pd.read_csv("/Volumes/Samsung SSD 990 PRO 1TB/data/dsl/development.csv")
+    df = pd.read_csv(ROOT_PATH / "development.csv")
     return df
 
 
 def get_test_data() -> pd.DataFrame:
-    df = pd.read_csv("/Volumes/Samsung SSD 990 PRO 1TB/data/dsl/evaluation.csv")
+    df = pd.read_csv(ROOT_PATH / "evaluation.csv")
     return df
