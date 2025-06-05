471eeaeb775b2f3ce682b964625eb5890e947d91
fpgmina <francesco.mina1986@gmail.com>
2025-06-05 10:51:49 -0400

    appease black

--- data/getters.py
+++ data/getters.py
@@ -5,8 +5,6 @@ import pathlib
 ROOT_PATH = pathlib.Path("/Volumes/Samsung SSD 990 PRO 1TB/data/dsl")
 
 
-
-
 def get_train_data() -> pd.DataFrame:
     df = pd.read_csv(ROOT_PATH / "development.csv")
     return df
--- train.py
+++ train.py
@@ -25,17 +25,20 @@ def objective(trial: optuna.trial.Trial) -> float:
          (for example, compute cross-validation score or validation loss).
         The objective function should then return a single value (the metric) for Optuna to minimize or maximize.
     """
-    n_estimators = trial.suggest_int(
-        "n_estimators", 50, 200
-    )  # integer from 50 to 200
+    n_estimators = trial.suggest_int("n_estimators", 50, 200)  # integer from 50 to 200
     max_depth = trial.suggest_int("max_depth", 2, 10)  # integer from 2 to 10
-    criterion = trial.suggest_categorical('criterion', ['squared_error', 'absolute_error'])
+    criterion = trial.suggest_categorical(
+        "criterion", ["squared_error", "absolute_error"]
+    )
 
     # The trial object effectively decides what value to try next for that parameter. After suggesting values, create a model with suggested hyperparameters
     model = RandomForestRegressor(
-        n_estimators=n_estimators, max_depth=max_depth, criterion=criterion, random_state=42, n_jobs=-1
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        criterion=criterion,
+        random_state=42,
+        n_jobs=-1,
     )
     # and evaluate it (use 3-fold cross-validation and return the mean accuracy)
     score = cross_val_score(model, X, y, cv=3, scoring="accuracy").mean()
     return score  # Optuna will try to maximize this score
-
